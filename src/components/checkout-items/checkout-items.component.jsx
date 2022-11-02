eab9087ae67e4de7395cf40683d88859db856415
gaurang-tcs <gaurangpatel.tcs@gmail.com>
2022-11-02 19:25:46 +0530

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-items/checkout-items.component.jsx
+++ src/components/checkout-items/checkout-items.component.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 
 import "./checkout-items.styles.scss";
+import { connect } from "react-redux";
 
-const CheckoutItems = ( {cartItem:{ imageUrl, price, name, quantity }}) => (
-  <div className="checkout-item">
-  <div className="image-container">
-    <img src={imageUrl} alt='item' />
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart.actions";
+
+const CheckoutItems = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { imageUrl, price, name, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;{" "}
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;{" "}
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-   <div className="remove">&#10005;</div>
-  </div>
-);
-export default CheckoutItems;
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+export default connect(null, mapDispatchToProps)(CheckoutItems);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -24,3 +24,4 @@ export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
     0
   )
 );
+
